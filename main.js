c515e68a886e654138a66f927f1ddce9792e509d
Alex Hultman <alexhultman@localhost.localdomain>
2016-01-14 05:46:43 +0100

    main.js: Display number of connections

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var lws = require("./build/Release/lws");
 
 var server = new lws.Server(3000);
+var numConnections = 0;
 
 server.on('connection', function (socket) {
-	console.log('Connection');
+	if (++numConnections % 50 == 0) {
+		console.log("[Connection] Current number of connections: " + numConnections);
+	}
 });
 
 server.on('message', function (socket, message) {
@@ -11,7 +14,10 @@ server.on('message', function (socket, message) {
 });
 
 server.on('close', function (socket) {
-	console.log('Disconnection');
+	if (--numConnections % 50 == 0) {
+		console.log("[Disconnection] Current number of connections: " + numConnections);
+	}
 });
 
+console.log("Running server on port 3000");
 server.run();
