c4f3fe2298deff14eefb9a6cc72bd12033cde71f
Alex Hultman <alexhultman@localhost.localdomain>
2016-02-13 09:45:39 +0100

    Update C++ example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,4 +1,4 @@
-/* This example is out of date */
+/* this is a simple echo server */
 
 #include <iostream>
 #include <string>
@@ -11,24 +11,18 @@ int main(int argc, char *argv[])
     lws::Server server(3000);
 
     server.onConnection([](lws::Socket socket) {
-        cout << "Connection" << endl;
-        string str = "some message";
-        socket.send(str, false);
-        str = "some other message";
-        socket.send(str, false);
-        str = "some third message";
-        socket.send(str, false);
+
     });
 
     server.onDisconnection([](lws::Socket socket) {
-        cout << "Disconnection" << endl;
+
     });
 
-    server.onMessage([](lws::Socket socket, std::string message) {
-        cout << "Message: " << message << endl;
+    server.onMessage([](lws::Socket socket, char *data, size_t length, bool binary) {
+        socket.send(data, length, binary);
     });
 
-    cout << "Running server on port 3000" << endl;
+    cout << "Running echo server on port 3000" << endl;
     server.run();
     return 0;
 }
