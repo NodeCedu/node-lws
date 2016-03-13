65fca8da02c07a0e74b3a5991d26e4bc6d571c4e
Alex Hultman <alexhultman@localhost.localdomain>
2016-03-13 02:01:38 +0100

    Initial ws-like wrapper

--- dist/lws.js
+++ dist/lws.js
@@ -1,1 +1,49 @@
+const EventEmitter = require('events');
+const util = require('util');
+
 module.exports = require('./lws_' + process.platform + '_' + process.versions.modules);
+
+function wsSocket(lwsSocket, server) {
+	EventEmitter.call(this);
+	var wsSocket = this;
+	this.lwsSocket = lwsSocket;
+	this.server = server;
+
+	this.send = function (message) {
+		if (wsSocket.lwsSocket != null) {
+			wsSocket.server.send(wsSocket.lwsSocket, new Buffer(message), false);
+		} else {
+			console.log('ERROR: Sending on closed socket!');
+		}
+	}
+}
+
+module.exports.wsServer = function wsServer(options) {
+	EventEmitter.call(this);
+	var server = new module.exports.Server(options);
+	var wsServer = this;
+
+	this.close = function () {
+		server.close();
+	};
+
+	server.on('connection', function (socket) {
+		var ws = new wsSocket(socket, server);
+		server.setUserData(socket, ws);
+		wsServer.emit('connection', ws);
+	});
+
+	server.on('message', function (socket, message, binary, bytesRemaining) {
+		var ws = server.getUserData(socket);
+		ws.emit('message', message);
+	});
+
+	server.on('close', function (socket) {
+		var ws = server.getUserData(socket);
+		ws.lwsSocket = null;
+		ws.emit('close');
+	});
+};
+
+util.inherits(wsSocket, EventEmitter);
+util.inherits(module.exports.wsServer, EventEmitter);
